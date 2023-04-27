dbaea2631ea2e463bd56babc157d120161a01aac
kineticstorytelling <kineticstorytelling@gmail.com>
2023-04-27 19:30:17 -0400

    'added invoice modal and two packages'

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { uid } from 'uid';
+import InvoiceModal from './InvoiceModal';
 import InvoiceItem from './InvoiceItem';
 const date = new Date();
 const today = date.toLocaleDateString('en-US', {
@@ -9,87 +10,87 @@ const today = date.toLocaleDateString('en-US', {
 });
 
 const InvoiceForm = () => {
-  // const [isOpen, setIsOpen] = useState(false);
-  const [tax, setTax] = useState('');
-  const [storeName, setStoreName] = useState('');
-  const [items, setItems] = useState([
-    {
-      id: uid(6),
-      name: '',
-      qty: 1,
-      price: '1.00',
-    },
-  ]);
+      // const [isOpen, setIsOpen] = useState(false);
+      const [tax, setTax] = useState('');
+      const [storeName, setStoreName] = useState('');
+      const [items, setItems] = useState([
+        {
+          id: uid(6),
+          name: '',
+          qty: 1,
+          price: '1.00',
+        },
+      ]);
 
-  // this prevents it from reloading with each state change
-  const reviewInvoiceHandler = (event:any) => {
-    event.preventDefault();
-    // @ts-ignore
-    setIsOpen(true);
-  };
+      // this prevents it from reloading with each state change
+      const reviewInvoiceHandler = (event:any) => {
+        event.preventDefault();
+        // @ts-ignore
+        setIsOpen(true);
+      };
 
-  const addItemHandler = () => {
-    const id = uid(6);
-    setItems((prevItem) => [
-      ...prevItem,
-      {
-        id: id,
-        name: '',
-        qty: 1,
-        price: '1.00',
-      },
-    ]);
-  };
+      const addItemHandler = () => {
+        const id = uid(6);
+        setItems((prevItem) => [
+          ...prevItem,
+          {
+            id: id,
+            name: '',
+            qty: 1,
+            price: '1.00',
+          },
+        ]);
+      };
 
-  const deleteItemHandler = (id:any) => {
-    setItems((prevItem) => prevItem.filter((item) => item.id !== id));
-  };
+      const deleteItemHandler = (id:any) => {
+        setItems((prevItem) => prevItem.filter((item) => item.id !== id));
+      };
 
-  const edtiItemHandler = (event:any) => {
-    const editedItem = {
-      id: event.target.id,
-      name: event.target.name,
-      value: event.target.value,
-    };
+      const edtiItemHandler = (event:any) => {
+        const editedItem = {
+          id: event.target.id,
+          name: event.target.name,
+          value: event.target.value,
+        };
 
-    const newItems = items.map((items) => {
-      for (const key in items) {
-        if (key === editedItem.name && items.id === editedItem.id) {
-     // @ts-ignore
-         items[key] = editedItem.value;
-        }
-      }
-      return items;
-    });
+        const newItems = items.map((items) => {
+          for (const key in items) {
+            if (key === editedItem.name && items.id === editedItem.id) {
+        // @ts-ignore
+            items[key] = editedItem.value;
+            }
+          }
+          return items;
+        });
 
-    setItems(newItems);
-  };
+        setItems(newItems);
+      };
 
-  const subtotal = items.reduce((previous, current) => {
-    if (current.name.trim().length > 0)
-     // @ts-ignore
-     return previous + Number(current.price * Math.floor(current.qty));
-    else return previous;
-  }, 0);
-    // @ts-ignore
-  const taxRate = (tax * subtotal) / 100;
-  
-  const total = subtotal + taxRate;
+      const subtotal = items.reduce((previous, current) => {
+        if (current.name.trim().length > 0)
+        // @ts-ignore
+        return previous + Number(current.price * Math.floor(current.qty));
+        else return previous;
+      }, 0);
+        // @ts-ignore
+      const taxRate = (tax * subtotal) / 100;
+      
+      const total = subtotal + taxRate;
 
   return (
-    <form
-      className="relative flex flex-col px-2 md:flex-row"
-      onSubmit={reviewInvoiceHandler}
-    >
-      <div className="opacity-90 my-6 flex-1 space-y-2  rounded-md bg-white p-4 shadow-sm sm:space-y-4 md:p-6">
-        <div className="flex flex-col justify-between space-y-2 border-b border-gray-900/10 pb-4 md:flex-row md:items-center md:space-y-0">
-{/* Date */}
+      <form
+        className="relative flex flex-col px-2 md:flex-row"
+        onSubmit={reviewInvoiceHandler}
+      >
+        <div className="opacity-90 my-6 flex-1 space-y-2  rounded-md bg-white p-4 shadow-sm sm:space-y-4 md:p-6">
+          <div className="flex flex-col justify-between space-y-2 border-b border-gray-900/10 pb-4 md:flex-row md:items-center md:space-y-0">
+        {/* Date */}
           <div className="flex space-x-2">
             <span className="font-bold">Current Date: </span>
             <span>{today}</span>
           </div>        
         </div>
-{/* Store */}
+        {/* Store */}
         <div className="text-lg">
           <label
             htmlFor="storeName"
@@ -141,7 +142,7 @@ const InvoiceForm = () => {
           Add Item
         </button>
 
-{/* Tax Input */}
+        {/* Tax Input */}
         <div className="py-2">
             <div className="py-2">
               <label className="text-sm font-bold md:text-base" htmlFor="tax">
@@ -165,20 +166,20 @@ const InvoiceForm = () => {
               </div>
             </div>
 
-{/* subtotal */}
+        {/* subtotal */}
         <div className="flex flex-col items-end space-y-2 pt-6">
           <div className="flex w-3/12 justify-between md:w-1/2">
             <span className="font-bold">Subtotal:</span>
             <span>${subtotal.toFixed(2)}</span>
           </div>
-{/* Tax */}
+        {/* Tax */}
           <div className="flex w-3/12 justify-between md:w-1/2">
             <span className="font-bold">Tax:</span>
             <span>
               ({tax || '0'}%)${taxRate.toFixed(2)}
             </span>
           </div>
-{/* Total */}
+        {/* Total */}
           <div className="flex w-3/12 justify-between border-t border-gray-900/10 pt-2 md:w-1/2">
             <span className="font-bold">Total:</span>
             <span className="font-bold">
@@ -187,6 +188,79 @@ const InvoiceForm = () => {
           </div>
         </div>
       </div>
+      
+      {/* The invoice modal */} 
+       <div className="basis-1/4 bg-transparent">
+        <div className="sticky top-0 z-10 space-y-4 divide-y divide-gray-900/10 pb-8 md:pt-6 md:pl-4">
+          <button
+            className="w-full rounded-md bg-blue-500 py-2 text-sm text-white shadow-sm hover:bg-blue-600"
+            type="submit"
+          >
+            Review Invoice
+          </button>
+          <InvoiceModal
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            invoiceInfo={{
+              invoiceNumber,
+              storeName,
+              subtotal,
+              taxRate,
+              total,
+              
+            }}
+            items={items}
+            onAddNextInvoice={addNextInvoiceHandler}
+          />
+          <div className="space-y-4 py-2">
+            <div className="space-y-2">
+              <label className="text-sm font-bold md:text-base" htmlFor="tax">
+                Tax rate:
+              </label>
+              <div className="flex items-center">
+                <input
+                  className="w-full rounded-r-none bg-white shadow-sm"
+                  type="number"
+                  name="tax"
+                  id="tax"
+                  min="0.01"
+                  step="0.01"
+                  placeholder="0.0"
+                  value={tax}
+                  onChange={(event) => setTax(event.target.value)}
+                />
+                <span className="rounded-r-md bg-gray-200 py-2 px-4 text-gray-500 shadow-sm">
+                  %
+                </span>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <label
+                className="text-sm font-bold md:text-base"
+                htmlFor="discount"
+              >
+                Discount rate:
+              </label>
+              <div className="flex items-center">
+                <input
+                  className="w-full rounded-r-none bg-white shadow-sm"
+                  type="number"
+                  name="discount"
+                  id="discount"
+                  min="0"
+                  step="0.01"
+                  placeholder="0.0"
+                  value={discount}
+                  onChange={(event) => setDiscount(event.target.value)}
+                />
+                <span className="rounded-r-md bg-gray-200 py-2 px-4 text-gray-500 shadow-sm">
+                  %
+                </span>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
       </div> 
     </form>
   );
