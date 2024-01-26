158b1f826c3444ab87cc1db2e302654f749fd10a
kineticstorytelling <kineticstorytelling@gmail.com>
2024-01-26 18:59:13 -0500

    changed titles and products to expense name

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,10 +5,10 @@ import UnitConverter from '../components/UnitConverter';
 import Big from '../assets/images/Jehlyo-big.png'
 import '../styles.css'
 
-const Products = () => {
+const Expenses = () => {
   return (
     <div className="min-h-screen bg-cover bg-gray-100" style={{backgroundImage: `url(${ Big })`}}>
-      <h1 className='text-gray-800 drop-shadow-lg text-5xl text-center uppercase font-bold p-5'>Jehlyo Items</h1>
+      <h1 className='text-gray-800 drop-shadow-lg text-5xl text-center uppercase font-bold p-5'>Mukbe Items</h1>
     <div className='flex p-5'>
 
     <SearchFilter />
@@ -20,4 +20,4 @@ const Products = () => {
     )
 }
 
-export default Products
+export default Expenses
